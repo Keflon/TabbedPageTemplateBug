b9bfde3b9de7ba5e2303a9abb5a45c1fbb97bcec
Keith Pickford <Keith@FunctionZero.com>
2023-04-13 20:41:11 +0100

    Add project files.

---
+++ TabbedPageTemplateBug/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace TabbedPageTemplateBug;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        var rootPage = new TabbedPage();
+        rootPage.ItemsSource = new List<object> { new Vm1(), new Vm2(), new Vm3() };
+        rootPage.ItemTemplate = new TabbedItemTemplateSelector();
+        MainPage = rootPage;
+    }
+    public class Vm1 { }
+    public class Vm2 { }
+    public class Vm3 { }
+    class TabbedItemTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is Vm1)
+                return new DataTemplate(() => new ContentPage() { Title = "Type 1" });
+            if (item is Vm2)
+                return new DataTemplate(() => new ContentPage() { Title = "Type 2" });
+            if (item is Vm3)
+                return new DataTemplate(() => new ContentPage() { Title = "Type 3" });
+
+            return null;
+        }
+    }
+}
+
---
+++ TabbedPageTemplateBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TabbedPageTemplateBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
